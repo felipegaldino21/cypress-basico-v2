e4a0252178ea9c2890299c446314290ef4a555a1
Felipe Galdino <felipegaldino21@gmail.com>
2024-02-05 20:03:42 -0300

    atualização src

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const phoneField = document.getElementById('phone')
 
 document.querySelector('#phone-checkbox')
   .addEventListener('change', function() {
-    i (this.checked) {
+    if (this.checked) {
       phoneLabelSpan.style.display = 'inline'
       phoneField.required = !isPhoneRequired
       isPhoneRequired = !isPhoneRequired
